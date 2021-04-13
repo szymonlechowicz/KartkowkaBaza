f2ec4f44a827d69578a8cd8f36c0b8e32138568b
Szymon Lechowicz <szymon.piotr.lechowicz@gmail.com>
2021-04-13 17:47:35 +0200

    Dodaj pliki projektów.

---
+++ KartkowkaBaza/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace BazaKartkowka
+{
+    public class Song
+    {
+        public int id { set; get; }
+
+        public string name { set; get; }
+
+        public string title { set; get; }
+
+        public override string ToString()
+        {
+            return "Wykonawca " + name + " wykonuje piosenke " + title;
+        }
+    }
+    public class SongDb : DbContext
+    {
+        public DbSet<Song> Songs { set; get; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SongDb db = new SongDb())
+            {
+                var song = new Song { name = "Future", title = "Mask off" };
+                db.Songs.Add(song);
+                db.SaveChanges();
+
+
+                var songs = (from s in db.Songs select s).ToList<Song>();
+                foreach (var s in songs)
+                {
+                    Console.WriteLine(s.ToString());
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
